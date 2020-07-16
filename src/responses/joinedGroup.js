d59c1cffb46d75cc1a271cd8e7cdd17e40d21087
Jackson Melcher <jackson.melcher@ringcentral.com>
2020-07-16 14:12:46 -0700

    Added a joined group text. Not finished yet

---
+++ src/responses/joinedGroup.js
@@ -0,0 +1,21 @@
+export const joinedGroup = {
+    attachments: [
+        {
+            type: 'Card',
+
+            author: {
+                name: 'Lunch Bot',
+            },
+            title: 'Instructions',
+            text:
+                "I'm Lunch Bot. I was created to make Lunch events. You can use me by sending me a direct message with the keyword **Lunch** " +
+                'followed by the team you want the event to be forwarded to.' +
+                'If you send just the word **Lunch** I will automatically create a.',
+
+            fields: [],
+            footnote: {
+                text: 'Created and maintained by RC on RC',
+            },
+        },
+    ],
+};
