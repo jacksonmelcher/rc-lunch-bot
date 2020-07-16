f3a13b69a0d731678cb94a208d0e981555a82d99
Jackson Melcher <jackson.melcher@ringcentral.com>
2020-07-16 11:45:41 -0700

    Added preview, some error handling, and a 'now' option

--- src/eventHandler.js
+++ src/eventHandler.js
@@ -15,46 +15,56 @@ const eventHandler = async (event) => {
 
 const handleMessage4Bot = async (event) => {
     const { text, bot, group, userId, message } = event;
-    const team = message.mentions.pop();
-    const start = moment();
-    const remainder = 15 - (start.minute() % 15);
-    const startTime = moment(start).add(remainder, 'minutes');
-    const endTime = moment(startTime).add(1, 'h');
 
-    const name = await getUser(event);
-
-    if (text.includes('lunch')) {
-        let obj = {
-            title: `${name} AFK - LUNCH`,
-            startTime: startTime,
-            endTime: endTime,
-            recurrence: 'None',
-            endingCondition: 'None',
-            creatorId: userId,
-            endingAfter: 1,
-            color: 'Orange',
-            allDay: false,
-        };
-
-        // console.log(obj);
-        try {
-            const res = await bot.rc.post(`/restapi/v1.0/glip/events`, obj);
-            // console.log(res.data);
-            await bot.sendMessage(team.id, {
-                text: `![:Person](${userId})`,
-                attachments: [
-                    {
-                        id: res.data.id,
-                        type: 'Event',
-                    },
-                ],
-            });
-        } catch (error) {
-            console.log(error.data);
-        }
+    if (!message.mentions) {
+        await bot.sendMessage(group.id, {
+            text: 'Please mention a team I can send the event to.',
+        });
+        return;
     }
 
-    await bot.sendMessage(group.id, { text: 'Sent' });
+    const info = await getUser(event);
+
+    let start = null;
+    let remainder = null;
+    let startTime = null;
+    let endTime = null;
+
+    console.log(info);
+
+    const team = message.mentions.pop();
+
+    if (text.includes('lunch') && text.includes('now')) {
+        startTime = moment().startOf('minute');
+        endTime = moment(startTime).add(1, 'h');
+    } else if (text.includes('lunch')) {
+        start = moment();
+        remainder = 15 - (start.minute() % 15);
+        startTime = moment(start).add(remainder, 'minutes');
+        endTime = moment(startTime).add(1, 'h');
+    }
+    let obj = {
+        title: `${info.name} AFK - LUNCH`,
+        startTime: startTime,
+        endTime: endTime,
+        recurrence: 'None',
+        endingCondition: 'None',
+        creatorId: userId,
+        endingAfter: 1,
+        color: 'Orange',
+        allDay: false,
+    };
+    let res = await createEvent(event, obj, team);
+    console.log(res.data.id);
+    await bot.sendMessage(group.id, {
+        text: 'sent',
+        attachments: [
+            {
+                id: res.data.id,
+                type: 'Event',
+            },
+        ],
+    });
 };
 
 const handleBotJoinedGroup = async (event) => {
@@ -67,8 +77,27 @@ const handleBotJoinedGroup = async (event) => {
 
 const getUser = async ({ bot, userId }) => {
     const { rc } = await bot.getUser(userId);
+    const { regionalSettings, name } = rc;
 
-    return rc.name;
+    return { name, regionalSettings };
+};
+
+const createEvent = async ({ bot, userId }, obj, team) => {
+    try {
+        const res = await bot.rc.post(`/restapi/v1.0/glip/events`, obj);
+        await bot.sendMessage(team.id, {
+            text: `![:Person](${userId})`,
+            attachments: [
+                {
+                    id: res.data.id,
+                    type: 'Event',
+                },
+            ],
+        });
+        return res;
+    } catch (error) {
+        console.log(error.data);
+    }
 };
 
 export default eventHandler;
