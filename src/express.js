d9f586ff0c9b6e7df3947c495053d093d770bd98
Jackson Melcher <jackson.melcher@ringcentral.com>
2020-07-16 16:16:40 -0700

    Trying to fix url error

--- src/express.js
+++ src/express.js
@@ -8,7 +8,7 @@ const handle = async (event) => {
 const app = createApp(handle);
 app.listen(process.env.PORT || 3000, () => console.log('Server is running...'));
 
-setInterval(
-    () => axios.put(`${process.env.RINGCENTRAL_CHATBOT_SERVER}/admin/maintain`),
-    86400000
-);
+// setInterval(
+//     () => axios.put(`${process.env.RINGCENTRAL_CHATBOT_SERVER}/admin/maintain`),
+//     86400000
+// );
